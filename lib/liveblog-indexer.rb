c8a1feef6516fd24a0b86bb3bddb68271b024f56
Your Name <you@example.com>
2015-11-13 13:03:53 +0000

    Liveblog pages can now be indexed recursively.

---
+++ lib/liveblog-indexer.rb
@@ -0,0 +1,106 @@
+#!/usr/bin/env ruby
+
+# file: liveblog-indexer.rb
+
+require 'xws'
+require 'json'
+require 'rxfhelper'
+
+
+class LiveBlogIndexer
+
+  def initialize(filepath='wordindex.json', urls_indexed: 'urls-indexed.json')
+
+    @wordindex_filepath, @urls_index_filepath = filepath, urls_indexed
+    @master = if filepath and File.exists? filepath then
+      JSON.parse(File.read(filepath))
+    else
+      {}
+    end
+
+    @xws = XWS.new
+    @url_index = {}    
+
+  end
+
+  def add_index(src)
+
+    doc = if src.is_a? String then
+      Rexle.new(RXFHelper.read(src).first )
+    else
+      src
+    end
+    
+    link = doc.root.element('summary/link/text()')
+    return unless link
+    
+    sections = doc.root.xpath 'records/section'
+
+    sections.each do |section|
+
+      url = "%s/#%s" % [link[/^https?:\/\/[^\/]+(.*)(?=\/$)/,1], \
+                                                       section.attributes[:id]]
+
+      h = @xws.scan section.element('details')
+      
+      h.each do |k, v|
+
+        word, count = k, v
+
+        keyword = @master[word]
+
+        if keyword then
+
+          keyword[url] = count
+          
+        else
+
+          @master[word] = {}
+          @master[word][url] = count
+
+        end # /keyword
+      end # /h
+    end # /section
+  end # /add_index
+  
+  def crawl(location)
+    
+    index_file location
+    save @wordindex_filepath
+    File.write @urls_index_filepath, @url_index.to_json
+    
+  end  
+
+  def save(filepath=nil)
+
+    File.write filepath, @master.to_json
+    puts 'saved ' + File.basename(filepath)
+
+  end
+  
+  private
+  
+  def index_file(location)
+
+    puts 'indexing : ' + location.inspect
+    doc = Rexle.new(RXFHelper.read(location).first)
+    summary = doc.root.element 'summary'
+    return unless summary
+    
+    result = add_index doc
+    return unless result
+
+    prev_day = summary.text 'prev_day'
+
+    if prev_day then
+      url = prev_day + 'formatted.xml'
+      @url_index[url] = {last_indexed: Time.now}
+      index_file(url) 
+    end
+  end
+  
+  def save_urlsindex(filepath)
+    
+  end
+
+end
---
+++ liveblog-indexer.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name = 'liveblog-indexer'
+  s.version = '0.2.0'
+  s.summary = 'This gem is under development. Generates a Liveblog indexed file in JSON format as well as Polyrex format.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/liveblog-indexer.rb']
+  s.add_runtime_dependency('xws', '~> 0.1', '>=0.1.1')
+  s.add_runtime_dependency('rxfhelper', '~> 0.2', '>=0.2.3')
+  s.signing_key = '../privatekeys/liveblog-indexer.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@r0bertson.co.uk'
+  s.homepage = 'https://github.com/jrobertson/liveblog-indexer'
+end
